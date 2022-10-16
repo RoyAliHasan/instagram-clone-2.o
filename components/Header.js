e0eeefa65ceb2a33ca1b6eccbd74b882fc9d7333
Roy Ali Hasan <royalihasan0987@gmail.com>
2022-10-16 11:23:32 +0500

    Project Setup

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='flex bg-slate-200 p-8 justify-cecter'>
+
+            <div className=''>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact us</li>
+                    <li>Register</li>
+
+                </ul>
+
+            </div>
+
+
+        </div>
+    )
+}
